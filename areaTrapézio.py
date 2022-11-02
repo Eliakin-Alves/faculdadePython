c2f2f9133b855749f904ad1385eadb65843f400d
Eliakin-Alves <56265454+Eliakin-Alves@users.noreply.github.com>
2022-11-01 21:45:11 -0300

    Add files via upload

---
+++ areaTrapézio.py
@@ -0,0 +1,7 @@
+b_maior = int(input("Digite a base maior"))
+b_menor = int(input("Digite a base menor"))
+altura = int(input("Digite a altura"))
+
+area = ((b_maior + b_menor) * altura)/2
+
+print(f"A area do trapézio é: {area}")
